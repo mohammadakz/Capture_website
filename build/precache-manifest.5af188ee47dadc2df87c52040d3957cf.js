693ab3fc8ddf6dfa76a52e13e855189a188c7183
Mohammad Akbarzadeh <41878043+mohammadakz@users.noreply.github.com>
2021-05-18 19:43:32 -0400

    “githubpages”

---
+++ build/precache-manifest.5af188ee47dadc2df87c52040d3957cf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1559775b20496db7c5639ef18a5bbf34",
+    "url": "/Capture_website/index.html"
+  },
+  {
+    "revision": "78dd05897091017ba288",
+    "url": "/Capture_website/static/js/2.f4fda213.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/Capture_website/static/js/2.f4fda213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9094a6a85ec19ea75a",
+    "url": "/Capture_website/static/js/main.fe35fae8.chunk.js"
+  },
+  {
+    "revision": "f5855f4cd0fc2383981f",
+    "url": "/Capture_website/static/js/runtime-main.c7bcb5c6.js"
+  },
+  {
+    "revision": "f85903d473cb19505e2aa6c63a0db00b",
+    "url": "/Capture_website/static/media/athlete-small.f85903d4.png"
+  },
+  {
+    "revision": "6d53898d42640dad92347101b160da7f",
+    "url": "/Capture_website/static/media/athlete2.6d53898d.png"
+  },
+  {
+    "revision": "f5c64a5f820c4ac31423ed0ee5d95c7c",
+    "url": "/Capture_website/static/media/clock.f5c64a5f.svg"
+  },
+  {
+    "revision": "f620483184a3efa896127ee80cab531c",
+    "url": "/Capture_website/static/media/diaphragm.f6204831.svg"
+  },
+  {
+    "revision": "e149f59c6323b18f98e987cd564e1fc9",
+    "url": "/Capture_website/static/media/good-times2.e149f59c.jpg"
+  },
+  {
+    "revision": "169a9b3ae8eff8e916dc7711c7546c64",
+    "url": "/Capture_website/static/media/goodtimes-small.169a9b3a.png"
+  },
+  {
+    "revision": "99a18d77af2a0e70784a842789ab9a34",
+    "url": "/Capture_website/static/media/home1.99a18d77.png"
+  },
+  {
+    "revision": "ee6a60799cceb9c60ce67b133ca6574c",
+    "url": "/Capture_website/static/media/home2.ee6a6079.png"
+  },
+  {
+    "revision": "d1749735b44aafa17334e7710263aac3",
+    "url": "/Capture_website/static/media/money.d1749735.svg"
+  },
+  {
+    "revision": "8c795ad57e55b06d76b8a73b27a13fd1",
+    "url": "/Capture_website/static/media/teamwork.8c795ad5.svg"
+  },
+  {
+    "revision": "74873026dbed7229712447d165a7a2fd",
+    "url": "/Capture_website/static/media/the-racer2.74873026.jpg"
+  },
+  {
+    "revision": "ca518f44c6e1ddc450654002971bb467",
+    "url": "/Capture_website/static/media/theracer-small.ca518f44.png"
+  }
+]);
